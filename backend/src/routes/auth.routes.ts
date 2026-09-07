0890c0cd035fafee6fa6a82ef32e81592e27f152
Pavan-1410 <pavansonawane004@gmail.com>
2026-09-07 22:03:29 +0530

    corrected user flow

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -7,7 +7,7 @@ import { loadApplicationUser } from "../middleware/user.middleware.js";
 const router = Router();
 
 router.get('/check',verifyFirebaseToken,loadApplicationUser,checkAuth)      // when user refresh
-router.get("/me", verifyFirebaseToken,loadApplicationUser, getMe);
+router.get("/me", verifyFirebaseToken, getMe);
 router.get("/admin/me",verifyFirebaseToken,loadApplicationUser,requireAdmin,getMe) // this is to ckeck the admin request
 export default router;
 
