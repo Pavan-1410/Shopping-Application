96c043be5f877802948783ef0f8f0338cd346ec9
Pavan-1410 <pavansonawane004@gmail.com>
2026-09-07 21:07:12 +0530

    added frontend domian

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5001;
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:4173",
-  "https://your-frontend.onrender.com",
+  "https://shopping-application-v4q2.vercel.app",
 ];
 
 app.use(
